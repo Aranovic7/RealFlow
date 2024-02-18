1d8aae1373041ffab2cd66cbec339746e36a11dc
Aran Ali <Aliaran2717@gmail.com>
2024-02-18 20:29:32 +0100

    test commit

---
+++ RealFlow/RealFlow/RealFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealFlowApp.swift
+//  RealFlow
+//
+//  Created by Aran Ali on 2024-02-18.
+//
+
+import SwiftUI
+
+@main
+struct RealFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
