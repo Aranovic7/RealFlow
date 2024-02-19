af6769a13c26e512e3ffd3f95ad80a5a2db39809
Aran Ali <Aliaran2717@gmail.com>
2024-02-19 19:35:43 +0100

    added RegisterScreen and updated LoginScreen

--- RealFlow/RealFlow/Screens/UnAuthenticated/RegisterScreen.swift
+++ RealFlow/RealFlow/Screens/UnAuthenticated/RegisterScreen.swift
@@ -1,5 +1,5 @@
 //
-//  LoginScreen.swift
+//  RegisterScreen.swift
 //  RealFlow
 //
 //  Created by Aran Ali on 2024-02-19.
@@ -7,24 +7,20 @@
 
 import SwiftUI
 
-struct LoginScreen: View {
+struct RegisterScreen: View {
     
     @State var usernameInput: String
     @State var passwordInput: String
     
+    @State var repeatPasswordInput: String
     var body: some View {
+        
         VStack{
             
             VStack{
                 
-                Text("Welcome to RealFlow")
+                Text("Create your account")
                     .font(.title)
-                    .bold()
-                    .padding()
-                
-                Spacer()
-                
-                Text("This app is made for you to have an easier communication with friends and family!")
                     .bold()
                     .padding()
                 
@@ -44,47 +40,40 @@ struct LoginScreen: View {
                     .padding(.horizontal)
                     .padding()
                 
+                TextField("Repeat password", text: $repeatPasswordInput)
+                    .padding(10)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(8)
+                    .padding(.horizontal)
+                    .padding()
+                
+                Spacer()
+                
                 Button(action: {
-                    print("Button 'sign in' was pressed")
+                    print("Button 'create account' was pressed")
                 }, label: {
-                    Text("Sign in")
+                    Text("Create account")
                         .bold()
                         .font(.title2)
                         .foregroundStyle(.white)
                         .frame(width: 250, height: 50)
                         .background(Color.black)
                         .clipShape(.rect(cornerRadius: 10))
+                        .padding()
                 })
                 
                 Spacer()
                 
-                Text("Don't have an account? Register now")
-                
-                Button(action: {
-                    print("Button 'register' was pressed")
-                }, label: {
-                    Text("Register")
-                        .bold()
-                        .font(.title2)
-                        .foregroundStyle(.white)
-                        .frame(width: 250, height: 50)
-                        .background(Color.black)
-                        .clipShape(.rect(cornerRadius: 10))
-                       
-                })
-                
-                
-                Spacer()
-                
-                Text("Made by Aran Ali")
-                  
+                Text("Already have an account? Sign in")
+                    .foregroundStyle(.blue)
+                    .onTapGesture {
+                        print("Go back to LoginScreen")
+                    }
             }
-           
-        
         }
     }
 }
 
 #Preview {
-    LoginScreen(usernameInput: "", passwordInput: "")
+    RegisterScreen(usernameInput: "", passwordInput: "", repeatPasswordInput: "")
 }
