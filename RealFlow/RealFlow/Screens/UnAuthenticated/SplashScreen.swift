ad12078e3ab26e03e41ae4d65673e9a34af50a80
Aranovic7 <aliaran2717@gmail.com>
2024-03-07 20:42:09 +0100

    ADDED: - SplashScreen

---
+++ RealFlow/RealFlow/Screens/UnAuthenticated/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  RealFlow
+//
+//  Created by Aran Ali on 2024-03-07.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
