6f6e2fd8a4580875b16eab65dd6115b9235a66c7
Aranovic7 <aliaran2717@gmail.com>
2024-03-18 12:26:55 +0100

    Added placeholder for LanguageSettings

---
+++ RealFlow/RealFlow/Screens/Authenticated/LanguageSettings.swift
@@ -0,0 +1,18 @@
+//
+//  LanguageSettings.swift
+//  RealFlow
+//
+//  Created by Aran Ali on 2024-03-18.
+//
+
+import SwiftUI
+
+struct LanguageSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LanguageSettings()
+}
