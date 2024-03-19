73682915e6f9373fb99ae564a93fa75c913f54b0
Aranovic7 <aliaran2717@gmail.com>
2024-03-19 12:51:53 +0100

    Added placeholder for ApperanceScreen

---
+++ RealFlow/RealFlow/Screens/Authenticated/ApperanceScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ApperanceScreen.swift
+//  RealFlow
+//
+//  Created by Aran Ali on 2024-03-19.
+//
+
+import SwiftUI
+
+struct ApperanceScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ApperanceScreen()
+}
