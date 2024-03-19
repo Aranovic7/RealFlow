74b053d412e601cd5d218585297874a66bd82c8f
Aranovic7 <aliaran2717@gmail.com>
2024-03-19 11:51:04 +0100

    Added placeholder for NotificationsScreen

---
+++ RealFlow/RealFlow/Screens/Authenticated/NotificationsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationsScreen.swift
+//  RealFlow
+//
+//  Created by Aran Ali on 2024-03-19.
+//
+
+import SwiftUI
+
+struct NotificationsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationsScreen()
+}
