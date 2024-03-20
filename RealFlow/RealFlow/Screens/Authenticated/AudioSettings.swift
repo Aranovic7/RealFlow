8ba1a6e7c3c70507283dfc2957dbff5d7aa03bcd
Aranovic7 <aliaran2717@gmail.com>
2024-03-20 13:36:48 +0100

    Added placeholder for AudioSettings

---
+++ RealFlow/RealFlow/Screens/Authenticated/AudioSettings.swift
@@ -0,0 +1,18 @@
+//
+//  AudioSettings.swift
+//  RealFlow
+//
+//  Created by Aran Ali on 2024-03-20.
+//
+
+import SwiftUI
+
+struct AudioSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AudioSettings()
+}
