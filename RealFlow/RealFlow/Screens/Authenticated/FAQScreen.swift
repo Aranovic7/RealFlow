956893d0502ee58076be337b204dde64cf811a1b
Aranovic7 <aliaran2717@gmail.com>
2024-03-25 19:37:56 +0100

    Added placeholder for FAQ screen

---
+++ RealFlow/RealFlow/Screens/Authenticated/FAQScreen.swift
@@ -0,0 +1,18 @@
+//
+//  FAQScreen.swift
+//  RealFlow
+//
+//  Created by Aran Ali on 2024-03-25.
+//
+
+import SwiftUI
+
+struct FAQScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FAQScreen()
+}
