7e291267c08a48a53d857fa057efe45563090eae
Aranovic7 <aliaran2717@gmail.com>
2024-04-01 17:21:13 +0200

    User can now log out

---
+++ RealFlow/RealFlow/Screens/Authenticated/SupportChatScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SupportChatScreen.swift
+//  RealFlow
+//
+//  Created by Aran Ali on 2024-03-29.
+//
+
+import SwiftUI
+
+struct SupportChatScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SupportChatScreen()
+}
