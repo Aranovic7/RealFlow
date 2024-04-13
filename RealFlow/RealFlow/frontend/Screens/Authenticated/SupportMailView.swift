9e90d87c3f048e1fdc68363555e6c564d1689388
Aranovic7 <aliaran2717@gmail.com>
2024-04-13 14:40:39 +0200

    Added new screen and a placeholder for it

--- RealFlow/RealFlow/frontend/Screens/Authenticated/SupportMailView.swift
+++ RealFlow/RealFlow/frontend/Screens/Authenticated/SupportMailView.swift
@@ -1,18 +1,18 @@
 //
-//  SupportChatScreen.swift
+//  SupportMailView.swift
 //  RealFlow
 //
-//  Created by Aran Ali on 2024-03-29.
+//  Created by Aran Ali on 2024-04-13.
 //
 
 import SwiftUI
 
-struct SupportChatScreen: View {
+struct SupportMailView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    SupportChatScreen()
+    SupportMailView()
 }
