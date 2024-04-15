b8134001860bd2ada23459cbef9faa37b46fc614
Aranovic7 <aliaran2717@gmail.com>
2024-04-15 21:06:19 +0200

    user can now change password

---
+++ RealFlow/RealFlow/frontend/Screens/Authenticated/ChangePasswordScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ChangePasswordScreen.swift
+//  RealFlow
+//
+//  Created by Aran Ali on 2024-04-15.
+//
+
+import SwiftUI
+
+struct ChangePasswordScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangePasswordScreen()
+}
