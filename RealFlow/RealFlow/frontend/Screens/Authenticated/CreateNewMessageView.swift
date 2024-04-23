f9195ad12d3b73db55d694c72f01c19bdd63a28a
Aranovic7 <aliaran2717@gmail.com>
2024-04-23 17:56:18 +0200

    Update: - Fetching all users in db with correct username + profilepic

---
+++ RealFlow/RealFlow/frontend/Screens/Authenticated/CreateNewMessageView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateNewMessageView.swift
+//  RealFlow
+//
+//  Created by Aran Ali on 2024-04-22.
+//
+
+import SwiftUI
+
+struct CreateNewMessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateNewMessageView()
+}
