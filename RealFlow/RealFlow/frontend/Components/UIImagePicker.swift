bd35c1b855caf6f85ed0685eed304457a30da680
Aranovic7 <aliaran2717@gmail.com>
2024-05-24 20:04:33 +0200

    User can now start conversations with different users and fetch correct data as well as last sent message

---
+++ RealFlow/RealFlow/frontend/Components/UIImagePicker.swift
@@ -0,0 +1,18 @@
+//
+//  UIImagePicker.swift
+//  RealFlow
+//
+//  Created by Aran Ali on 2024-05-23.
+//
+
+import SwiftUI
+
+struct UIImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIImagePicker()
+}
