401b5099c30c74275b6adfdbebedd4b0fd7c28ee
Aranovic7 <aliaran2717@gmail.com>
2024-05-26 17:33:17 +0200

    Minimum viable product

---
+++ RealFlow/RealFlow/frontend/Components/ImagePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePicker.swift
+//  RealFlow
+//
+//  Created by Aran Ali on 2024-05-25.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePicker()
+}
---
+++ RealFlow/RealFlow/frontend/Screens/Authenticated/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  RealFlow
+//
+//  Created by Aran Ali on 2024-05-25.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageView()
+}
